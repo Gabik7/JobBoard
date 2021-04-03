1650de4fc3136d610a44c52703dda37f57351a10
Gabriel <gabi.falis@gmail.com>
2021-04-03 17:07:34 +0200

    Initial Commit

---
+++ JobBoard/JobBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobBoardApp.swift
+//  JobBoard
+//
+//  Created by Gabriel Falis on 03/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct JobBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
